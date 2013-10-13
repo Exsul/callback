5ad48d9ec42d9be0cd0cd884f495b58b8544374f
Kirill Berezin <enelar@develop-project.ru>
2013-10-13 13:34:49 +0400

    Testing toasts

--- src/net/exsul/callback/MyActivity.java
+++ src/net/exsul/callback/MyActivity.java
@@ -1,7 +1,9 @@
 package net.exsul.callback;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MyActivity extends Activity {
     /**
@@ -11,5 +13,10 @@ public class MyActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        Context context = getApplicationContext();
+        CharSequence text = "Callback application installed";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 }
