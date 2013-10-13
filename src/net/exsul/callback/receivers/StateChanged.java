332912ca86486b1f846f96ba8fc067bf13e84869
Kirill Berezin <enelar@develop-project.ru>
2013-10-13 17:55:19 +0400

    Finished coding, all shows

--- src/net/exsul/callback/receivers/StateChanged.java
+++ src/net/exsul/callback/receivers/StateChanged.java
@@ -1,13 +1,19 @@
 package net.exsul.callback.receivers;
 
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.telephony.TelephonyManager;
 import android.widget.Toast;
 import net.exsul.callback.Switch;
 import net.exsul.callback.SwitchString;
+
+import java.util.Date;
 
 public class StateChanged extends BroadcastReceiver {
     static Switch<String> state_monitor = new SwitchString();
@@ -27,7 +33,7 @@ public class StateChanged extends BroadcastReceiver {
 
         String state = bundle.getString(TelephonyManager.EXTRA_STATE);
         if (OnChangedState(state)) {
-           CallEnded(context, saved_phone, state_monitor.PreviosDuration());
+           CallEnded(context, state_monitor.PreviosDuration());
         }
     }
 
@@ -37,10 +43,15 @@ public class StateChanged extends BroadcastReceiver {
       return state_monitor.ChangedTo(TelephonyManager.EXTRA_STATE_OFFHOOK, TelephonyManager.EXTRA_STATE_IDLE);
     }
 
-    private void CallEnded( Context context, String phone, Long duration ) {
-        String info = "END:" + phone + "\nDUR:" + duration;
+    private void CallEnded( Context context, Long duration ) {
+        if (duration > 20)
+            return;
+        Toast.makeText(context, "Pre activity", Toast.LENGTH_LONG).show();
 
-        Toast.makeText(context, info, Toast.LENGTH_LONG).show();
+        Intent i = new Intent();
+        i.setClassName("net.exsul.callback", "net.exsul.callback.DialogActivity");
+        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        i.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+        context.startActivity(i);
     }
-
 }
